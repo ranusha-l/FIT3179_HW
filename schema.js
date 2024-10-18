1ef5ddeb3e79a746d1869ed2ede647d77afa720a
ranusha-l <rliy0007@student.monash.edu>
2024-10-18 14:14:55 +0800

    all commit

---
+++ schema.js
@@ -0,0 +1,11 @@
+
+var vg_1 = "map.vg.json";
+var vg_2 = "line_chart.vg.json";
+
+vegaEmbed('#map', vg_1).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+    }).catch(console.error);
+
+vegaEmbed('#line_chart', vg_2).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+    }).catch(console.error);
